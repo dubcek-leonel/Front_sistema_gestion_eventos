731a8ef9cd3831fadef6ffe33f75271c68bdcfe5
onetofux <mortisedsteam40@gmail.com>
2025-10-31 12:45:45 -0500

    home agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './shared/components/login/login';
 import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 import { StudentDashboard } from './pages/student-dashboard/student-dashboard';
+import {Home} from './pages/home/home';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'admin-dashboard', component: AdminDashboard },
   { path: 'student-dashboard', component: StudentDashboard },
